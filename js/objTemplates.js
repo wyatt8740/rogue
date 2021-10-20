23b0b96270f93530e0382dae7eb325bf579591ff
wyatt8740 <wyattfward@gmail.com>
2021-10-20 11:31:03 -0400

    initial commit

---
+++ js/objTemplates.js
@@ -0,0 +1,15 @@
+/* default bare-bones templates for commonly used objects */
+
+var templates = {
+  npc: {
+    
+    race: 0, /* to-do: define global type constants */
+    stats: {
+      dex: 0, 
+      str: 0,
+      agi: 0,
+      int: 0, /* yes, you can have it be called int and JS doesn't complain */
+      wis: 0
+    }
+  }
+};
