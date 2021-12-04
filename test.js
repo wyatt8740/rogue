97a4de52792b630149576c06e6c72a8feb552742
wyatt8740 <wyattfward@gmail.com>
2021-12-03 20:17:51 -0500

    blah.js

---
+++ test.js
@@ -0,0 +1,20 @@
+var o = {
+	width: 80,
+	height: 25
+}
+var d = new ROT.Display(o);
+/*document.body.appendChild(d.getContainer()); */
+window.onload = function() {
+  document.getElementById("stdout").innerHTML=d;
+
+for (var i=0; i<o.width; i++) {
+	for (var j=0; j<o.height; j++) {
+		if (!i || !j || i+1 == o.width || j+1 == o.height) {
+			d.draw(i, j, "#", "gray");
+		} else {
+			d.draw(i, j, ".", "#666");
+		}
+	}
+}
+d.draw(o.width >> 1, o.height >> 1, "@", "goldenrod");
+}
