c3ed5cb940039f8ca59063d7744383b215bfe694
wyatt8740 <wyattfward@gmail.com>
2021-12-05 13:01:12 -0500

    rooms.js

---
+++ js/rooms.js
@@ -0,0 +1,5 @@
+/* This file contains definitions for setting up the various rooms in the game.
+   When a room is entered, a function here is called to build the room
+   description, add items, enemies, and whatever else the room needs to
+   function. This function should also contain functions for adjacent rooms
+   and any other necessary logic. */
