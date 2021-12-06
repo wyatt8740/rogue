5a1609c470baeb64e07ac4b0f3ffb08d1b86950a
bigbluekatze <mrhuber91@gmail.com>
2021-12-05 21:51:21 -0500

    rooms phase 1

--- js/rooms.js
+++ js/rooms.js
@@ -3,3 +3,39 @@
    description, add items, enemies, and whatever else the room needs to
    function. This function should also contain functions for adjacent rooms
    and any other necessary logic. */
+
+function buildRoom(variety){
+   var room;
+   if (variety === misc){
+      var kind = 4*Math.random();
+      if(kind == 1){
+         room = treasure_room();
+      }
+      if(kind == 2){
+         room = boss_room();
+      }
+      if(kind == 3){
+         room = encounter_room();
+      }
+      if(kind == 4){
+         room = exit_room();
+      }
+   }
+   return room;
+}
+
+function treasure_room(room){
+   //presumably this will generate items randomly
+   
+}
+
+function boss_room(room){
+   //dragons or something await, maybe smaller mobs?
+}
+function encounter_room(room){
+   //standard room, with assorted obstacles and treasures, maybe sudden death events
+}
+
+function exit_area(room){
+   //a way to the next area. How this works is TBD.
+}
